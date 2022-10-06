2d7085e0eea1c8fe7160f08cf797521a6318cfe4
Doppledouble <ian.suryadi@ui.ac.id>
2022-10-06 14:35:43 +0700

    lab 5

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,8 @@
+import json
 import datetime
 from django.shortcuts import render, redirect
 from wishlist.models import BarangWishlist
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.core import serializers 
 from django.urls import reverse
 from django.contrib import messages
@@ -21,7 +22,28 @@ def show_wishlist(request):
     'last_login': request.COOKIES['last_login'],
 }
     return render(request, "wishlist.html", context)
+@login_required(login_url='/wishlist/login/')
+def show_wishlist_ajax(request):
+    context = {
+        'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, "wishlist_ajax.html", context)
+    
+def submit_wishlist(request):
+    if request.method == 'POST':
+        nama_barang = request.POST['nama_barang']
+        deskripsi = request.POST['deskripsi']
+        harga_barang = request.POST['harga_barang']
+        wishlist_instance = BarangWishlist(nama_barang=nama_barang, deskripsi=deskripsi, harga_barang=harga_barang)
+        wishlist_instance.save()
+        data = {
+            "message": 'Successfully submitted'
+        }
+        json_object = json.dumps(data, indent = 4) 
 
+        return JsonResponse(json.loads(json_object))
+    return render(request, 'create_wishlist.html')    
+    
 def show_xml(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
